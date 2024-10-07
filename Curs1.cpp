c6738d7ce8a0f07aa0c3dc75adfc3462c0c78d49
Alin Zamfiroiu <alinz10@yahoo.com>
2024-10-07 16:38:02 +0300

    Add project files.

---
+++ Curs1.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+
+using namespace std;
+
+struct Pereche {
+	int x;
+	int y;
+
+	int suma() {
+		return x + y;
+	}
+	int inmultire() {
+		return x * y;
+	}
+};
+
+int suma(Pereche p) {
+	return p.x + p.y;
+}
+int inmultire(Pereche p) {
+	return p.x * p.y;
+}
+int diferenta(Pereche p) {
+	return p.x - p.y;
+}
+
+int main() {
+	Pereche p1;
+	cin >> p1.x;
+	cin >> p1.y;
+	cout << p1.suma();
+	cout << endl << p1.inmultire() << endl << endl;
+
+	Pereche p2;
+	cin >> p2.x;
+	cin >> p2.y;
+	cout << suma(p2);
+
+	return 10;
+}
